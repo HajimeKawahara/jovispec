8e34dbca5c87af66a0c81490a5ef3861d17d811d
Hajime Kawahara <divrot@gmail.com>
2024-05-13 07:40:44 +0900

    test

--- analysis/irdjupiter.py
+++ analysis/irdjupiter.py
@@ -3,49 +3,50 @@
 import matplotlib.pyplot as plt
 import pathlib
 import numpy as np
+from pyird.utils import irdstream
 
 basedir = pathlib.Path("~/jovispec/analysis/data/H").expanduser()
 
-band = "h"  #'h' or 'y'
-mmf = "mmf2"  #'mmf1' (comb fiber) or 'mmf2' (star fiber)
-skipLFC = (
-    False  # if False, uncertainties are output. mmf1 of y band must be reduced first.
-)
-
-from pyird.utils import irdstream
-
-## FLAT
+# Specifies directories
 datadir = basedir / "raw/"
 anadir = basedir / "reduc/"
 
-# flat
+# flat and tracing apertures
 flat = irdstream.Stream2D("flat", datadir, anadir, rawtag="2017011", fitsid=[8140833, 8140945, 8141330], rotate=True, detector_artifact=True)
-
-# target 34325
-target = irdstream.Stream2D("target", datadir, anadir, rawtag="2017011", fitsid=[8034325], rotate=True)
-
-#dark
-dark = irdstream.Stream2D("dark", datadir, anadir, rawtag="2017011", fitsid=[8133836], rotate=True)
-
-#Th-Ar wavelength calibration data
-thar=irdstream.Stream2D("thar",datadir,anadir,rawtag="2017011",fitsid=[8143952, 8144142, 8144237, 8144415],rotate=True)
-
-# mask from flat
 trace_mmf = flat.aptrace(cutrow=580, nap=21)
 trace_mask = trace_mmf.mask()
+## note that we do not apply trace_mmf.choose_mmf1_aperture nor trace_mmf.choose_mmf1_aperture
+## because there is only a single mmf on the detector
 
-# dark
+# dark and bias subtraction
 from pyird.image.bias import bias_subtract_image
 from pyird.image.hotpix import identify_hotpix_sigclip
+dark = irdstream.Stream2D("dark", datadir, anadir, rawtag="2017011", fitsid=[8133836], rotate=True)
 median_image = dark.immedian()
 im_subbias = bias_subtract_image(median_image)
 hotpix_mask = identify_hotpix_sigclip(im_subbias)
 
-#
-trace_mmf.mmf2()
+# generates the normalized flat
+flat.trace = trace_mmf
+flat.clean_pattern(trace_mask=trace_mask,extin='', extout='_cp', hotpix_mask=hotpix_mask)
+flat.imcomb = True # median combine
+flat.flatten(hotpix_mask=hotpix_mask)
+df_flatn = flat.apnormalize()
 
-#wavelength calibration
+# Th-Ar wavelength calibration
+thar=irdstream.Stream2D("thar",datadir,anadir,rawtag="2017011",fitsid=[8143952, 8144142, 8144237, 8144415],rotate=True)
 thar.trace = trace_mmf
 thar.clean_pattern(trace_mask=trace_mask,extin='', extout='_cp', hotpix_mask=hotpix_mask)
 thar.calibrate_wavelength()
 
+# target reduction
+target = irdstream.Stream2D("target", datadir, anadir, rawtag="2017011", fitsid=[8034325], rotate=True)
+target.info = True
+target.trace = trace_mmf
+target.clean_pattern(trace_mask=trace_mask,extin='', extout='_cp', hotpix_mask=hotpix_mask)
+
+# flat fielding
+target.apext_flatfield(df_flatn,hotpix_mask=hotpix_mask)
+
+# simliar to the IRAF task "dispcor"
+target.dispcor(master_path=thar.anadir,extin='_flnhp')
