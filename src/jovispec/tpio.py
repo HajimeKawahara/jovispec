9dd4768bb2826f8c07a2a4007110b426fb004be8
Hajime Kawahara <divrot@gmail.com>
2024-08-13 16:41:02 +0900

    tpio

--- src/jovispec/tpio.py
+++ src/jovispec/tpio.py
@@ -2,6 +2,14 @@ import pandas as pd
 import pkg_resources
 
 def read_tpprofile_jupiter(tpfile=None):
+    """read temperature-pressure profile of Jupiter, made by atmstruct.py
+    
+    Args:
+        tpfile (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
     if tpfile is None:
         path = pkg_resources.resource_filename("jovispec", "jupiter_data/")
         tpfile = path + "jupiter_atm.txt"
